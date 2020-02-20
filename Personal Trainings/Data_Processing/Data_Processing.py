0ceca7de2624ed5d8822e2d1524d85c4c0854979
GABRIEL-VARGAS-DAMBROSKI <51279088+GABRIEL-VARGAS-DAMBROSKI@users.noreply.github.com>
2020-02-20 16:34:26 -0300

    Novos Estudos

--- Personal Trainings/Data_Processing/Data_Processing.py
+++ Personal Trainings/Data_Processing/Data_Processing.py
@@ -13,7 +13,7 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 #iloc => [linha, coluna] -Pegando todas as linhas.
 X = dataset.iloc[:, :-1].values     # Colocando todas as linhas e colunas em um Array(.values) X com exceção da útlima coluna
-Y = dataset.iloc[:, -1:].values     # Colocando todas as linhas da última coluna em um Array(.values) Y
+Y = dataset.iloc[:, 3].values     # Colocando todas as linhas da última coluna em um Array(.values) Y
 
 ''' TAKING CARE OF THE MISSING DATA '''
 from sklearn.preprocessing import Imputer
@@ -34,3 +34,13 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
 ''' SPLITTING THE DATASET INTO THE TRAIN SET AND TEST SET '''
+from sklearn.model_selection import train_test_split
+# Separando a base em duas. Uma será a de treino e a outra será de teste
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+
+''' Feature Scaling '''
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
